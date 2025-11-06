bd9c9206d9aefdfe9f68bbbe2843918949bfe8ca
kripal <thakurkripalsingh6@gmail.com>
2025-11-06 16:59:51 +0530

    some other files

---
+++ math.js
@@ -0,0 +1,10 @@
+const sum = (a,b)=> a+b;
+const add= (a,b)=>a*b;
+const PI=3.14;
+
+const obj= {
+    sum : sum,
+    add: add,
+    PI: PI
+};
+module.exports =obj;
---
+++ script.js
@@ -0,0 +1,14 @@
+// let n =5;
+
+// for(let i=0; i<=5; i++){
+//     console.log("helloworld"," ",i);
+// }
+// console.log("bye!");
+
+// let args= process.argv;
+
+// for(let i=2; i<args.length; i++){
+//     console.log("hello to ", args[i]);
+// }
+let fruits= require("./fruits");
+console.log(fruits);
